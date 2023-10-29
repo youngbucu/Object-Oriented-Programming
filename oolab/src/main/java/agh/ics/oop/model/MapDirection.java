068046d5fd1a2cdbbbdabd4dcbb7869e921140f4
Lukasz <lwilanskiwilan@gmail.com>
2023-10-29 22:25:17 +0100

    Lab2 Poprawka

--- oolab/src/main/java/agh/ics/oop/model/MapDirection.java
+++ oolab/src/main/java/agh/ics/oop/model/MapDirection.java
@@ -1,93 +1,35 @@
 package agh.ics.oop.model;
 
 public enum MapDirection {
-    NORTH,
-    SOUTH,
-    WEST,
-    EAST;
+    NORTH(0, "Północ", 0, 1),
+    EAST(1, "Wschód", 1, 0),
+    SOUTH(2, "Południe", 0, -1),
+    WEST(3, "Zachód", -1, 0);
+
+    private final int index;
+    private final String description;
+    private final Vector2d vector;
+
+    MapDirection(int index, String description, int x, int y) {
+        this.index = index;
+        this.description = description;
+        this.vector = new Vector2d(x, y);
+    }
 
     @Override
     public String toString() {
-        switch (this) {
-            case EAST -> {
-                return "Wschód";
-            }
-            case WEST -> {
-                return "Zachód";
-            }
-            case NORTH -> {
-                return "Północ";
-            }
-            case SOUTH -> {
-                return "Południe";
-            }
-            default -> {
-                return null;
-            }
-
-        }
+        return description;
     }
 
     public MapDirection next() {
-        switch (this) {
-            case EAST -> {
-                return SOUTH;
-            }
-            case WEST -> {
-                return NORTH;
-            }
-            case NORTH -> {
-                return EAST;
-            }
-            case SOUTH -> {
-                return WEST;
-            }
-            default -> {
-                return null;
-            }
-
-        }
+        return values()[(this.index + 1) % values().length];
     }
 
     public MapDirection previous() {
-        switch (this) {
-            case EAST -> {
-                return NORTH;
-            }
-            case WEST -> {
-                return SOUTH;
-            }
-            case NORTH -> {
-                return WEST;
-            }
-            case SOUTH -> {
-                return EAST;
-            }
-            default -> {
-                return null;
-            }
-
-        }
+        return values()[(this.index + values().length - 1) % values().length];
     }
 
     public Vector2d toUnitVector() {
-        switch (this) {
-            case EAST -> {
-                return new Vector2d(1, 0);
-            }
-            case WEST -> {
-                return new Vector2d(-1, 0);
-            }
-            case NORTH -> {
-                return new Vector2d(0, 1);
-            }
-            case SOUTH -> {
-                return new Vector2d(0, -1);
-            }
-            default -> {
-                return null;
-            }
-
-        }
+        return vector;
     }
 }
